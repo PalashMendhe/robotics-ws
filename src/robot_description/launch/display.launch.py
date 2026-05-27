8ae1bd6b11d885f0c554ced58bb788f8813f2d1a
PalashMendhe <palashmendhe777@gmail.com>
2026-05-27 19:46:44 +0530

    Week2 Day8: URDF robot model with all links, joints, inertia

---
+++ src/robot_description/launch/display.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('robot_description'),
+        'urdf', 'robot.urdf.xacro')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{
+                'robot_description': Command(['xacro ', urdf_file])
+            }]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', os.path.join(
+                get_package_share_directory('robot_description'),
+                'config', 'robot.rviz')]
+        ),
+    ])
