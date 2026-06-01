486aff7fdd1237d7396d894187f133f2d09da9d6
PalashMendhe <palashmendhe777@gmail.com>
2026-06-01 20:42:58 +0530

    built a node that can will make the robot travel in square shape. also faced problem while building the package using unorthodox methods(for me)

--- src/week1_nodes/src/tf2_broadcaster.cpp
+++ src/week1_nodes/src/tf2_broadcaster.cpp
@@ -3,7 +3,7 @@
 #include "tf2_ros/static_transform_broadcaster.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2/LinearMath/Quaternion.hpp"
-#include <cmath>
+#include <cmath> 
 
 class TF2BroadcasterNode : public rclcpp::Node {
 public:
